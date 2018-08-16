945f951ef45346fed752c8aa574058dfa990894b
Julian Somoza <jsomoza@cnea.gob.ar>
2018-08-16 16:12:00 -0300

    Modificado

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,9 @@
 # name: Set X-Frame-Options correctly
 # about: Set site headers
 # version: 1
-# authors: Tom
+# authors: Julian
 
 Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
-Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => 'http://www.muselive.com'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => '*'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS, DELETE'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'})
